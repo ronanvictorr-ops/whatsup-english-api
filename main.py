2aa0436b67574bb6f6d97f01bd19c5b72b33c7a0
Ronan Victor <ronanvictorr@gmail.com>
2026-06-09 11:31:58 -0300

    Add JWT authentication

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 # IMPORTAÇÕES
 # ==========================================
 
+import token
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 import bcrypt
+from jose import jwt
+from datetime import datetime, timedelta    
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError   
+
 
 from database import SessionLocal, engine, Base
 from models import StudentDB, ProgressDB
@@ -15,6 +23,13 @@ Base.metadata.create_all(bind=engine)
 
 # Inicializa a aplicação FastAPI
 app = FastAPI()
+SECRET_KEY = "whatsup-english-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="login"
+)
 
 
 # ==========================================
@@ -33,7 +48,41 @@ class Login(BaseModel):
     email: str
     password: str
 
+def create_access_token(data: dict):
 
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+def get_current_user(token: str = Depends(oauth2_scheme)):
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+
+    except JWTError:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Token inválido"
+        )
 # Resposta do quiz
 class QuizAnswer(BaseModel):
     answer: str
@@ -124,10 +173,16 @@ def login(data: Login):
                 detail="Senha incorreta"
             )
 
+        token = create_access_token(
+            {
+                "student_id": student.id,
+                "email": student.email
+            }
+        )
+
         return {
-            "message": "Login realizado com sucesso",
-            "student_id": student.id,
-            "name": student.name
+            "access_token": token,
+            "token_type": "bearer"
         }
 
     finally:
@@ -265,4 +320,12 @@ def ranking():
 
     finally:
         db.close()
+
+@app.get("/me")
+def me(user = Depends(get_current_user)):
+
+    return {
+        "message": "Usuário autenticado",
+        "user": user
+    }
         
